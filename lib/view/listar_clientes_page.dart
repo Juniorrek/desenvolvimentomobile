997700edfa706192f36891e15fbfc0c0e1a79310
Juniorrek <juniorrek@hotmail.com>
2023-12-05 11:18:51 -0300

    Adicionar validação cliente com pedidos

--- lib/view/listar_clientes_page.dart
+++ lib/view/listar_clientes_page.dart
@@ -1,6 +1,8 @@
 import 'package:desenvolvimentomobile/helper/error.dart';
 import 'package:desenvolvimentomobile/model/cliente.dart';
+import 'package:desenvolvimentomobile/model/pedido.dart';
 import 'package:desenvolvimentomobile/repositories/cliente_repository.dart';
+import 'package:desenvolvimentomobile/repositories/pedido_repository.dart';
 import 'package:desenvolvimentomobile/routes/routes.dart';
 import 'package:desenvolvimentomobile/widgets/drawer.dart';
 import 'package:flutter/material.dart';
@@ -45,14 +47,22 @@ class _ListarClientesPageState extends State<ListarClientesPage> {
     return tempLista;
   }
 
-  void _removerCliente(int id) async {
+  void _removerCliente(Cliente cliente) async {
     try {
-      ClienteRepository repository = ClienteRepository();
-      await repository.remover(id).then((value) {
-        _refreshList();
+      PedidoRepository pedidoRepository = PedidoRepository();
+      List<Pedido> pedidos = await pedidoRepository.listarPorCpf(cliente.cpf);
+
+      if (pedidos.length > 0) {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Cliente $id removido com sucesso.')));
-      });
+          SnackBar(content: Text('Não é possível excluir esse cliente pois ele tem pedidos.')));
+      } else {
+        ClienteRepository repository = ClienteRepository();
+        await repository.remover(cliente.id!).then((value) {
+          _refreshList();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Cliente ${cliente.id} removido com sucesso.')));
+        });
+      }
     } catch (exception) {
       showError(context, "Erro removendo cliente", exception.toString());
     }
@@ -121,7 +131,7 @@ class _ListarClientesPageState extends State<ListarClientesPage> {
                 TextButton(
                   child: Text("Sim"),
                   onPressed: () {
-                    _removerCliente(b.id!);
+                    _removerCliente(b);
                     Navigator.of(context).pop();
                   },
                 ),
